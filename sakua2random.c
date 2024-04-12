010250dfcc68b09a46f38d157ebd8bf4b3bf7a88
Anoop Kumar Narayanan <anoop.kn@live.in>
2024-04-12 15:19:54 +0530

    Add files via upload

--- sakua2random.c
+++ sakua2random.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <stdlib.h>
 
 #include "sakua-dominopasswordgen1.h"
 #include "sakua-dominopasswordgen1_1.h"
@@ -9,43 +10,50 @@
 #include "sakua-dominopasswordgen2_1.h"
 #include "sakua-dominopassword.h"
 
-#define _FILE_OFFSET_BITS=64
-#define __USE_FILE_OFFSET64    1
+#define _FILE_OFFSET_BITS   64
+#define __USE_FILE_OFFSET64 1
 
 
-int main(int argc, char *argv) {
-
-    int years = 4;
-    int pin = 1;
+int main(int argc, char *argv[]) {
 
 	struct domino_password *dp = 0;
 	char *s = 0, *k =0;
 
-	fprintf( stderr, "Generating for %d years, with pin = %d \n", years, pin );
-	fflush(stderr);
+	int n    = 0;
+	int pin  = 0;
+	int algo = 0;
+
+	if( argc < 4 ){
+		printf( "Usage: %s <algo> <Start of pin> <Number of strings> \n", argv[0]);
+		printf( "	Algo, 0 - 1_0, 1 - 1_1, 2 - 2_0, 3 - 2_1 \n");
+		return 0;
+	} else {
+		algo = atoi( argv[1] );
+		pin = atoi( argv[2] );
+		n  = atoi( argv[3] );
+		printf( "Algo = %d, Start of pin = %d, Number of strings = %d \n", algo, pin, n);
+	}
+
 
 	dp = 0; s = 0; k = 0;
 	dp = domino_password_new();
-	dp->function_algo_setup( dp, 1 );
+	dp->function_algo_setup( dp, algo );
 	dp->algo->function_set_pin( dp->algo, pin );
 	dp->algo->incremental = 0;
-	fprintf( stderr, "Generating for %d years, with pin = %d \n", years, pin );
 	fflush(stderr);
 	dp->function_set_gen_seed( dp, 0 );
 	dp->algo->function_fastforward( dp->algo, dp->shuffled );
 	dp->algo->incremental = 1;
-
-	fprintf( stderr, "Generating for %d years, with pin = %d \n", years, pin );
 	fflush(stderr);
 
 	FILE *fp = fopen("readable_random-1_1-01.txt", "w+");
-	#define MULTIPLES 31622400 //3600x24X366
 	int i = 0;
-	for( i = 0; i < MULTIPLES * years ; i++ ){
+	for( i = 0; i < n; i++ ){
            fflush(stderr);
            dp->algo->function_set_pin( dp->algo, ++pin );
            dp->function_gen_shuffled( dp );
            fprintf( fp, "%s\n", dp->shuffled );
+		if( pin % 10000 == 0 )
            fprintf( stderr, "INFO: Pin = %d \n", pin );
            fflush(fp);
 	}
