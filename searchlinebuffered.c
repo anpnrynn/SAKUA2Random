250a734f0f991ddddc1626d56aed6b0bc7bc1263
Anoop Kumar Narayanan <anoop.kn@live.in>
2024-04-13 17:34:29 +0530

    Add files via upload

--- searchlinebuffered.c
+++ searchlinebuffered.c
@@ -23,82 +23,114 @@ fline[0] =0;
 //fscanf( stdin, "%108s", fline );
 fline[108]= 0;
 
-
-
 int first = 0;
 int readline = atoi ( argv[2] );
 int offset = atoi( argv[3] );
 int length = atoi( argv[4] );
 int substring = atoi( argv[5] );
+
 printf(" Searching for string on line %d, at offset %d and of length %d \n", readline, offset, length );
 printf(" Substring search %s \n", substring?"ON":"OFF" );
 
-for(;;) {
-    char line[127];
-    fgets(line, 127, f);
-    lineNumber++;
+#define BUFSIZE 109 * 2048
+
+char filebuf[BUFSIZE];
+int nlines = 0;
+while( ( nlines = fread( filebuf, 109, 2048, f ) ) > 0 ) {
+int i = 0;
+char *line = filebuf;
+	if(  filebuf[nlines *109 -1 ] != '\n' )  {
+		printf(" Structured read error \n");
+		return 0;
+	}
+for(i=0;i<nlines;i++) {
+	if( line[108] != '\n' )
+	{
+		printf(" EOL not found \n");
+		return 0;
+	}	
+
     line[108] = 0;
+    lineNumber++;
+
     if (!feof(f)) {
     } else {
-        break;
+		printf("EOF\n");
+		goto loopexit;
     }
     if( lineNumber % 1000000 == 0 ){
 	    printf(" Searched %d lines \n", lineNumber );
-		printf(" String = %s \n", line );
+		printf( " String = '%s' \n", line );
     }
 
     if( lineNumber == readline  ){
 	    first = 1;
 	    strcpy( fline, line );
 	    fline[108] = 0;
-		printf(" Line to scan for = '%s'\n", fline );
+		printf(" Line to scan for = '%s', %d , %d \n", fline, lineNumber, readline );
 
     } else if ( first ==  1 ) {
 		int j = 0;
 		while( j < 108-length ) {
     		if( strncmp ( &line[j] , &fline[offset], length ) == 0 ){
 	    		printf(" Matching string found at line : %d , %d\n", lineNumber, offset+j);
-				printf(" Matching string = %s \n", line );
+				printf(" Matching string : %s \n", line );
 				//fclose(f);
 				//return 0;
     		} 
 			j++;
 		}
 	}
+	line = filebuf + (i+1) * 109;
+	}
 }
+
+loopexit:
+
 
 printf(" Looping from begining \n");
 fseek( f, SEEK_SET, 0 );
 int ln = lineNumber;
 lineNumber=0;
-
 int n = 0;
-for(;n<readline;) {
-    char line[127];
-    fgets(line, 127, f);
+while( ( nlines = fread( filebuf, 109, 2048, f ) ) > 0 ) {
+	if(  filebuf[nlines *109 -1 ] != '\n' )  {
+		printf(" Structured read error \n");
+		return 0;
+	}
+int i = 0;
+char *line = filebuf;
+for(i=0;i<nlines && n<readline; i++) {
+	if( line[108] != '\n' )
+	{
+		printf(" EOL not found \n");
+		return 0;
+	}	
+    line[108] = 0;
 	ln++;
     lineNumber++;
-    line[108] = 0;
+
     if (!feof(f)) {
     } else {
-        break;
+		goto loopexit1;
     }
+
     if( ln % 1000000 == 0 ){
 	    printf(" Searched %d lines \n", ln );
-		printf(" String = %s \n", line );
+		printf( " String = '%s' \n", line );
     }
 
     if( lineNumber == readline  ){
 	    first = 1;
 	    strcpy( fline, line );
 	    fline[108] = 0;
-		break;
+		goto loopexit1;
     } else {
 		int j = 0;
 		while( j < 108-length ) {
     		if( strncmp ( &line[j] , &fline[offset], length ) == 0 ){
 	    		printf(" Matching string found at line : %d , %d\n", lineNumber, offset+j);
-				printf(" Matching string = %s \n", line );
+				printf(" Matching string : %s \n", line );
 				//fclose(f);
 				//return 0;
     		} 
@@ -106,7 +138,11 @@ for(;n<readline;) {
 		}
 	}
 	n++;
+	line = filebuf + (i+1)*109;
 }
+}
+
+loopexit1:
 
 printf(" No matching string found \n");
 
